c77e439e24fac808f04710052a078f0b803ff24c
shyamsaisatish <41366406+shyamsaisatish@users.noreply.github.com>
2020-10-21 11:22:51 +0530

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -2,7 +2,7 @@ package assignment2;
 public class Tester {
 	public static void main(String[] args)
 	{
-	Invoice invoice  = new Invoice("1", "Bag", 2, 300.0);
+	Invoice invoice  = new Invoice("1", "Watch", 4, 1000.0);
 	String k=invoice.toString();
 	System.out.println(k);
 	}
