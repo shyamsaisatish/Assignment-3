3d1d595105b46d5adcd9f19fe6342afc1f85fccb
shyamsaisatish <41366406+shyamsaisatish@users.noreply.github.com>
2020-10-21 09:04:08 +0530

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,9 @@
+package assignment2;
+public class Tester {
+	public static void main(String[] args)
+	{
+	Invoice invoice  = new Invoice("1", "Bag", 2, 300.0);
+	String k=invoice.toString();
+	System.out.println(k);
+	}
+}
